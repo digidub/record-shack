ea9df19913a05e56969901d93f60f1d7e011ff4b
Sholto <madein.mania@gmail.com>
2021-10-05 15:41:51 +0100

    basic index display function

---
+++ controllers/recordController.js
@@ -0,0 +1,23 @@
+var Record = require('../models/record');
+var Artist = require('../models/artist');
+var Genre = require('../models/genre');
+var async = require('async');
+
+exports.index = function (req, res) {
+  async.parallel(
+    {
+      record_count: function (callback) {
+        Record.countDocuments({}, callback);
+      },
+      artist_count: function (callback) {
+        Artist.countDocuments({}, callback);
+      },
+      genre_count: function (callback) {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    function (err, results) {
+      res.render('index', { title: 'Local Library Home', error: err, data: results });
+    }
+  );
+};
