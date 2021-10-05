12d9c962388c36ea707b9fe806f4394458cc16c9
Sholto <madein.mania@gmail.com>
2021-10-05 10:54:15 +0100

    basic record schema

---
+++ models/record.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var RecordSchema = new Schema({
+  title: { type: String, required: true },
+  artist: { type: Schema.Types.ObjectId, ref: 'Artist', required: true },
+  description: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
+});
+
+RecordSchema.virtual('url').get(function () {
+  return '/catalog/record/' + this._id;
+});
+
+module.exports = mongoose.model('Record', RecordSchema);
