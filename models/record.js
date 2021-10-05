16459618c5b7071dfd8c688437953d37fcc08495
Sholto <madein.mania@gmail.com>
2021-10-05 10:58:59 +0100

    description > condition

--- models/record.js
+++ models/record.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var RecordSchema = new Schema({
   title: { type: String, required: true },
   artist: { type: Schema.Types.ObjectId, ref: 'Artist', required: true },
-  description: { type: String, required: true },
+  condition: { type: String, required: true },
   genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
 });
 
