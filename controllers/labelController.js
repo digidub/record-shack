5b411b59f6a4edc6a539e42024bcaf62429df2b0
Sholto <madein.mania@gmail.com>
2021-10-06 10:38:13 +0100

    label detail controller and view

--- controllers/labelController.js
+++ controllers/labelController.js
@@ -1,4 +1,6 @@
 var Label = require('../models/label');
+var Record = require('../models/record');
+var async = require('async');
 
 exports.label_list = function (req, res, next) {
   Label.find()
@@ -9,4 +11,28 @@ exports.label_list = function (req, res, next) {
       }
       res.render('label_list', { title: 'Label List', label_list: list_labels });
     });
+};
+
+exports.label_detail = function (req, res, next) {
+  async.parallel(
+    {
+      label: function (callback) {
+        Label.findById(req.params.id).exec(callback);
+      },
+      label_records: function (callback) {
+        Record.find({ label: req.params.id }).populate('artist genre label').exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.label == null) {
+        var err = new Error('Label not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('label_detail', { title: 'Label Detail', label: results.label, label_records: results.label_records });
+    }
+  );
 };
