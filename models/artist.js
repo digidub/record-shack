642dc696bd78697054e14b5195cf9b1bc33c3948
Sholto <madein.mania@gmail.com>
2021-10-06 13:40:22 +0100

    added capitalize middleware

--- models/artist.js
+++ models/artist.js
@@ -4,6 +4,10 @@ var Schema = mongoose.Schema;
 var ArtistSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
 });
+ArtistSchema.pre('save', function (next) {
+  this.name.charAt(0).toUpperCase() + this.name.slice(1).toLowerCase();
+  next();
+});
 
 ArtistSchema.virtual('url').get(function () {
   return '/catalog/artist/' + this._id;
