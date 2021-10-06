7ae692036b0e3db8071e356333561c4e32460572
Sholto <madein.mania@gmail.com>
2021-10-06 11:43:06 +0100

    artist detail controller and view

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -12,3 +12,27 @@ exports.artist_list = function (req, res, next) {
       res.render('artist_list', { title: 'Artist List', artist_list: list_artists });
     });
 };
+
+exports.artist_detail = function (req, res, next) {
+  async.parallel(
+    {
+      artist: function (callback) {
+        Artist.findById(req.params.id).exec(callback);
+      },
+      artists_records: function (callback) {
+        Record.find({ artist: req.params.id }, 'title summary').exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.artist == null) {
+        var err = new Error('artist not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('artist_detail', { title: 'Artist Detail', artist: results.artist, artist_records: results.artists_records });
+    }
+  );
+};
