929e4ceb6e154d35011410a048f3f84384ff3c31
Sholto <madein.mania@gmail.com>
2021-10-06 08:44:46 +0100

    added to record schema

--- models/record.js
+++ models/record.js
@@ -4,8 +4,11 @@ var Schema = mongoose.Schema;
 var RecordSchema = new Schema({
   title: { type: String, required: true },
   artist: { type: Schema.Types.ObjectId, ref: 'Artist', required: true },
+  year: { type: Number },
   condition: { type: String, enum: ['NM', 'VG+', 'VG', 'G+'], required: true },
-  genre: [{ type: Schema.Types.ObjectId, ref: 'Genre', required: true }],
+  genre: { type: Schema.Types.ObjectId, ref: 'Genre', required: true },
+  label: { type: Schema.Types.ObjectId, ref: 'Label', requried: true },
+  quantity: { type: Number, min: 1, required: true },
 });
 
 RecordSchema.virtual('url').get(function () {
