b92bf28c1a52267eab5f734c9d8d3b5aeef4a1e2
Sholto <madein.mania@gmail.com>
2021-10-06 09:23:43 +0100

    genre list control function

---
+++ controllers/genreController.js
@@ -0,0 +1,12 @@
+var Genre = require('../models/genre');
+
+exports.genre_list = function (req, res, next) {
+  Genre.find()
+    .sort([['name', 'ascending']])
+    .exec(function (err, list_genres) {
+      if (err) {
+        return next(err);
+      }
+      res.render('genre_list', { title: 'genre List', genre_list: list_genres });
+    });
+};
