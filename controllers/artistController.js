cc692f1a9ada09d20180a289593b4d9963278e47
Sholto <madein.mania@gmail.com>
2021-10-08 10:22:31 +0100

    rewrote detail to use async await

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,6 +1,5 @@
 var Artist = require('../models/artist');
 var Record = require('../models/record');
-var async = require('async');
 
 exports.artist_list = function (req, res, next) {
   Artist.find()
@@ -13,26 +12,8 @@ exports.artist_list = function (req, res, next) {
     });
 };
 
-exports.artist_detail = function (req, res, next) {
-  async.parallel(
-    {
-      artist: function (callback) {
-        Artist.findById(req.params.id).exec(callback);
-      },
-      artists_records: function (callback) {
-        Record.find({ artist: req.params.id }, 'title summary').exec(callback);
-      },
-    },
-    function (err, results) {
-      if (err) {
-        return next(err);
-      }
-      if (results.artist == null) {
-        var err = new Error('artist not found');
-        err.status = 404;
-        return next(err);
-      }
-      res.render('artist_detail', { title: 'Artist Detail', artist: results.artist, artist_records: results.artists_records });
-    }
-  );
+exports.artist_detail = async function (req, res, next) {
+  const artist = await Artist.findById(req.params.id);
+  const artistsRecords = await Record.find({ artist: req.params.id }, 'title summary');
+  res.render('artist_detail', { title: 'Artist Detail', artist, artist_records: artistsRecords });
 };
