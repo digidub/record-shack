35adfaf161e889fe77505aa80c3bea621716bcb7
Sholto <madein.mania@gmail.com>
2021-10-11 08:57:13 +0100

    genre create get and post

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 var Genre = require('../models/genre');
 var Record = require('../models/record');
 var Artist = require('../models/artist');
+const { body, validationResult } = require('express-validator');
 
 exports.genre_list = function (req, res, next) {
   Genre.find()
@@ -22,3 +23,38 @@ exports.genre_detail = async function (req, res, next) {
     console.error(err);
   }
 };
+
+exports.genre_create_get = function (req, res, next) {
+  res.render('genre_form', { title: 'Create Genre' });
+};
+
+exports.genre_create_post = [
+  body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    var genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+      return;
+    } else {
+      Genre.findOne({ name: req.body.name }).exec(function (err, foundGenre) {
+        if (err) {
+          return next(err);
+        }
+        if (foundGenre) {
+          res.redirect(foundGenre.url);
+        } else {
+          genre.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(genre.url);
+          });
+        }
+      });
+    }
+  },
+];
