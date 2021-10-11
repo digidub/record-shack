bdb6c0afcca0b19c647270efc69ac5e39d231f44
Sholto <madein.mania@gmail.com>
2021-10-11 11:09:15 +0100

    enable genre delete get and post

--- routes/catalog.js
+++ routes/catalog.js
@@ -25,8 +25,8 @@ router.get('/labels', label_controller.label_list);
 
 router.get('/genre/create', genre_controller.genre_create_get);
 router.post('/genre/create', genre_controller.genre_create_post);
-// router.get('/genre/:id/delete', genre_controller.genre_delete_get);
-// router.post('/genre/:id/delete', genre_controller.genre_delete_post);
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
+router.post('/genre/:id/delete', genre_controller.genre_delete_post);
 // router.get('/genre/:id/update', genre_controller.genre_update_get);
 // router.post('/genre/:id/update', genre_controller.genre_update_post);
 router.get('/genre/:id', genre_controller.genre_detail);
