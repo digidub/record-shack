29f4646a59ca5a43542b3f7eb50e3262d07ab99a
Sholto <madein.mania@gmail.com>
2021-10-11 09:58:45 +0100

    logic for updating existing record

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -128,3 +128,81 @@ exports.record_create_post = [
     }
   },
 ];
+
+exports.record_update_get = async function (req, res, next) {
+  const findGenres = async () => await Genre.find();
+  const findFormats = async () => await Format.find();
+  const findRecordToUpdate = async () => await Record.findById(req.params.id).populate('artist').populate('genre').populate('label');
+
+  const genres = await findGenres();
+  const recordToUpdate = await findRecordToUpdate();
+  const formats = await findFormats();
+  console.log(recordToUpdate);
+
+  for (let allGenres = 0; allGenres < genres.length; allGenres++) {
+    for (let recordsGenres = 0; recordsGenres < recordToUpdate.genre.length; recordsGenres++) {
+      if (genres[allGenres]._id.toString() === recordToUpdate.genre[recordsGenres]._id.toString()) {
+        genres[allGenres].checked = 'true';
+      }
+    }
+  }
+  await res.render('record_form', { title: 'Update existing record', genres, formats, record: recordToUpdate });
+};
+
+exports.record_update_post = [
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === 'undefined') req.body.genre = [];
+      else req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  body('title', 'Title must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('artist', 'Artist must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('label', 'Label must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('condition', 'Condition must not be empty').trim().isLength({ min: 1 }).escape(),
+  body('format', 'Format must not be empty').trim().isLength({ min: 1 }).escape(),
+  body('quantity', 'Quantity must be positive').trim().isFloat({ min: 1 }).escape(),
+  body('genre.*').escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const record = new Record({
+      title: req.body.title,
+      artist: req.body.artist,
+      label: req.body.label,
+      condition: req.body.condition,
+      quantity: req.body.quantity,
+      format: req.body.format,
+      genre: typeof req.body.genre === 'undefined' ? [] : req.body.genre,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      let genres = await Genre.find();
+      let formats = await Format.find();
+      for (let i = 0; i < genres.length; i++) {
+        if (record.genre.indexOf(genres[i]._id) > -1) {
+          genres[i].checked = 'true';
+        }
+        await res.render('record_form', {
+          title: 'Create record',
+          genres: genres,
+          record: record,
+          errors: errors.array(),
+          formats: formats,
+        });
+        return;
+      }
+    } else {
+      Record.findByIdAndUpdate(req.params.id, record, {}, function (err, therecord) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(therecord.url);
+      });
+    }
+  },
+];
