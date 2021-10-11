6025b59e6c6dc2fb0f5cc64119ae790206004804
Sholto <madein.mania@gmail.com>
2021-10-11 10:54:54 +0100

    remove needless routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -17,13 +17,9 @@ router.post('/record/:id/update', record_controller.record_update_post);
 router.get('/record/:id', record_controller.record_detail);
 router.get('/records', record_controller.record_list);
 
-// router.get('/artist/:id/delete', artist_controller.artist_delete_get);
-// router.post('/artist/:id/delete', artist_controller.artist_delete_post);
 router.get('/artist/:id', artist_controller.artist_detail);
 router.get('/artists', artist_controller.artist_list);
 
-// router.get('/label/:id/delete', label_controller.label_delete_get);
-// router.post('/label/:id/delete', label_controller.label_delete_post);
 router.get('/label/:id', label_controller.label_detail);
 router.get('/labels', label_controller.label_list);
 
@@ -36,8 +32,6 @@ router.post('/genre/create', genre_controller.genre_create_post);
 router.get('/genre/:id', genre_controller.genre_detail);
 router.get('/genres', genre_controller.genre_list);
 
-// router.get('/format/:id/delete', format_controller.format_delete_get);
-// router.post('/format/:id/delete', format_controller.format_delete_post);
 router.get('/format/:id', format_controller.format_detail);
 router.get('/formats', format_controller.format_list);
 
